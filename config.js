72b3d15287f9ad70986ad13b1f513d7022464704
rferreirasul-arch <rferreira.sul@gmail.com>
2026-09-22 17:44:00 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Com os dois campos vazios, o app roda em MODO DEMONSTRAÇÃO (salva só no aparelho).
 window.APP_CONFIG = {
   // URL do "App da Web" do Google Apps Script (termina em /exec)
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbxUtPicb0aZYW4XMN0Mfzs6B_4EktIqmwMxfWpS9QE9ufTkWE-79PyaVPvP_SPnHiDn/exec",
   // ID do cliente OAuth do Google Cloud (termina em .apps.googleusercontent.com)
-  CLIENT_ID: "",
+  CLIENT_ID: "997988874349-65tdu4nmrtj1p4sgg291ftg4b1dfna7i.apps.googleusercontent.com",
 };
